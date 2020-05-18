88d138cf43476471e7bec0754b8d9e523807c1bb
Connorrichter4 <Connorrichter4@gmail.com>
2020-05-18 11:37:36 -0500

    Added delete code functionality

--- src/components/CodeInfo/codeInfo.js
+++ src/components/CodeInfo/codeInfo.js
@@ -3,6 +3,7 @@ import { Link, Redirect } from 'react-router-dom';
 
 function CodeInfo(props) {
 	const [code, setCode] = useState(null);
+	const [deleted, setDeleted] = useState(false);
 
 	useEffect(() => {
 		fetch(`http://localhost:4000/${props.match.params.id}`)
@@ -13,6 +14,18 @@ function CodeInfo(props) {
 			});
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
+
+	const deleteCode = (event) => {
+		fetch(`http://localhost:4000/${props.match.params.id}`, {
+			method: 'DELETE',
+		}).then(() => {
+			setDeleted(true);
+		});
+	};
+
+	if (deleted) {
+		return <Redirect to='/' />;
+	}
 
 	if (!code) {
 		return <div>Loading...</div>;
@@ -25,6 +38,7 @@ function CodeInfo(props) {
 			<p>{code.body}</p>
 			<p>{code.description}</p>
 			<Link to={`${code._id}/edit`}>Edit</Link>
+			<button onClick={deleteCode}>Delete</button>
 		</>
 	);
 }
