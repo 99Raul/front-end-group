e9c89e3f3e8033fc9ddd78d4302f87443ee7ec18
Connorrichter4 <Connorrichter4@gmail.com>
2020-05-20 12:29:12 -0500

    updating styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,22 +5,22 @@ import './Home.css';
 
 function Home(props) {
 	const { searchString, setSearchString, authToken } = props;
+	console.log(authToken);
 
 	return (
 		<>
 			<div className='link-container'>
 
-			<div className='link1'>
+			{authToken === null && <div className='link1'>
 				<Link to='/signup'>Sign Up</Link>
-			</div>
+			</div>}
 			<div className='link2'>
 				<Link to='/create'>Create New Code</Link>
 			</div>
-			<div className='link3'>
+			{authToken === null && <div className='link3'>
 				<Link to='/login'>Login</Link>
+			</div>}
 			</div>
-			</div>
-			{authToken !== null && <p>{authToken.user}</p>}
 			<h1>Home Page</h1>
 			<CodeList searchString={searchString} setSearchString={setSearchString} />
 		</>
